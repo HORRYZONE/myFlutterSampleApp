f4f4efbf4bf9310319a58e7b63ffaada31f7f6a4
HORRYZONE <danishfitri131@gmail.com>
2023-08-20 04:51:15 +0800

    change primary swatch for theme color

--- sampleflutterapp/lib/main.dart
+++ sampleflutterapp/lib/main.dart
@@ -10,7 +10,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
-      theme: ThemeData(brightness: Brightness.light),
+      theme: ThemeData(brightness: Brightness.light, primarySwatch: Colors.deepPurple),
       darkTheme: ThemeData(brightness: Brightness.dark),
       themeMode: ThemeMode.system,
       home: AppHome(),
