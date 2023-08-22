3a9c3fe810443e312c0086381a01e74fd3207747
HORRYZONE <danishfitri131@gmail.com>
2023-08-23 05:40:16 +0800

    call onBoardingWidget to the screen

---
+++ sampleflutterapp/lib/features/authentication/screens/onboarding_screen/onboarding_page_widget.dart
@@ -0,0 +1,45 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../constant/sizes.dart';
+import '../../models/model_onboarding.dart';
+
+class onBoardingPageWidget extends StatelessWidget {
+  const onBoardingPageWidget({
+    super.key,
+    required this.model,
+  });
+
+  final OnBoardingModel model;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(tDefaultSize),
+      color: model.bgColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          SvgPicture.asset(model.image, height: model.height * 0.5,),    //set height of image to 50% of screen
+          Column(
+            children: [
+              Text(
+                model.title,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              Text(
+                model.subTitle,
+                style: Theme.of(context).textTheme.labelMedium,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+          Text(model.counterText, style: Theme.of(context).textTheme.titleMedium,),
+          SizedBox(height: 50.0,)
+        ],
+      ),
+    );
+  }
+}
--- sampleflutterapp/lib/features/authentication/screens/onboarding_screen/onboarding_screen.dart
+++ sampleflutterapp/lib/features/authentication/screens/onboarding_screen/onboarding_screen.dart
@@ -6,14 +6,44 @@ import 'package:sampleflutterapp/constant/image_strings.dart';
 import 'package:sampleflutterapp/constant/sizes.dart';
 import 'package:sampleflutterapp/constant/text_strings.dart';
 import 'package:sampleflutterapp/features/authentication/models/model_onboarding.dart';
+import 'package:sampleflutterapp/features/authentication/screens/onboarding_screen/onboarding_page_widget.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size; //check screen size
 
-    final size = MediaQuery.of(context).size;     //check screen size
+    final pages = [
+      onBoardingPageWidget(
+        model: OnBoardingModel(
+            image: tOnboardingImage1,
+            title: tOnboardingTitle1,
+            subTitle: tOnboardingSubTitle1,
+            counterText: tOnboardingCounter1,
+            bgColor: tOnBoardingPage1Color,
+            height: size.height),
+      ),
+      onBoardingPageWidget(
+        model: OnBoardingModel(
+            image: tOnboardingImage2,
+            title: tOnboardingTitle2,
+            subTitle: tOnboardingSubTitle2,
+            counterText: tOnboardingCounter2,
+            bgColor: tOnBoardingPage2Color,
+            height: size.height),
+      ),
+      onBoardingPageWidget(
+        model: OnBoardingModel(
+            image: tOnboardingImage3,
+            title: tOnboardingTitle3,
+            subTitle: tOnboardingSubTitle3,
+            counterText: tOnboardingCounter3,
+            bgColor: tOnBoardingPage3Color,
+            height: size.height),
+      ),
+    ];
     return Scaffold(
       body: Stack(
         children: [
@@ -33,5 +63,3 @@ class OnBoardingScreen extends StatelessWidget {
     );
   }
 }
-
-
